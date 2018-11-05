6d6d6e7b9d2df1ef4fb9d46a662e6fac4a7968b7
DougCaswell <dougcaswell@gmail.com>
2018-11-05 14:30:46 -0700

    finished project

--- practice.js
+++ practice.js
@@ -6,6 +6,9 @@
 //plus the value of the name parameter.
 
 //Code here
+function greeting (name) {
+return ("Hello, " + name)
+};
 
 //////////////////PROBLEM 2////////////////////
 
@@ -13,6 +16,9 @@
 //Name it newGreeting.
 
 //Code Here
+var newGreeting = function (name) {
+  return ("Hello, " + name)
+  };
 
 //////////////////PROBLEM 3////////////////////
 
@@ -20,6 +26,7 @@
 //Name it finalGreeting.
 
 //Code Here
+var finalGreeting = name => ("Hello, " + name);
 
 //////////////////PROBLEM 4////////////////////
 
@@ -27,6 +34,7 @@
 //"apples", "milk", "eggs", "bread"
 
 //Code Here
+var groceries = ["apples", "milk", "eggs", "bread"];
 
 //Write a function called doubleCheck that takes in an array
 //as a parameter.
@@ -35,6 +43,12 @@
 //doubleCheck should return the array.
 
 //Code Here
+function doubleCheck (array) {
+  if (array.indexOf("chocolate") == -1) {
+    array.push("chocolate")
+    return array
+  }
+}
 
 //////////////////PROBLEM 5////////////////////
 
@@ -44,20 +58,29 @@
 //and goodBoy (a boolean).
 
 //Code Here
+var dog = {
+  name: "Rufus",
+  color: "Reddish-Brown",
+  age: 4,
+  goodBoy: true
+};
 
 //...access the dog's name from the object and assign it to a
 //variable called devMountainClassPet.
 
 //Code Here
+var devMountainClassPet = dog.name;
 
 //Add a method to dog called bark.
 //The value of bark should be a function that returns the string "Woof woof".
 
 //Code Here
+dog.bark = () => "Woof woof";
 
 //Store the result of invoking the bark method in a variable called ruff.
 
 //Code Here
+var ruff = dog.bark();
 
 //////////////////PROBLEM 6////////////////////
 
@@ -71,6 +94,15 @@
 //Return mySum.
 
 //Code Here
+function looper (array) {
+  var mySum = 0;
+  function sum (value) {mySum = mySum + value};
+  for (i=0; i < array.length; i++ ) {
+    if (array[i] % 2 ) {sum(array[i])}
+    else if (array[i] >= 100) {sum(array[i])};
+  }
+  return mySum;
+}
 
 //////////////////PROBLEM 7////////////////////
 
@@ -84,11 +116,15 @@ function math(num1, num2, callback) {
 //returns the result of adding them together.
 
 //Code Here
+function add (num1, num2) {
+  return (num1 + num2)
+};
 
 //Now invoke math, passing in the numbers 3 and 4, and your add function,
 //storing the result in the variable mathSum.
 
 //Code Here
+var mathSum = math(3,4,add)
 
 //////////////////PROBLEM 8////////////////////
 
@@ -104,6 +140,9 @@ function sampleCallbackTwo() {
 }
 
 //Code Here
+function invoker (callback) {
+  return callback();
+}
 
 //////////////////PROBLEM 9////////////////////
 
@@ -127,16 +166,16 @@ function pond() {
 //as strings.
 
 //This array should contain the variable names (as strings) accessible in the global scope.
-let globalScope = ["duck", "sailorDuck", "rubberDuck", "realDuck"];
+let globalScope = ["duck"];
 
 //This array should contain the variable names (as strings) accessible in the bathroom function.
-let bathroomScope = ["duck", "sailorDuck", "rubberDuck", "realDuck"];
+let bathroomScope = ["duck", "rubberDuck"];
 
 //This array should contain the variable names (as strings) accessible in the bathtub function.
-let bathtubScope = ["duck", "sailorDuck", "rubberDuck", "realDuck"];
+let bathtubScope = ["duck", "sailorDuck", "rubberDuck"];
 
 //This array should contain the variable names (as strings) accessible in the pond function.
-let pondScope = ["duck", "sailorDuck", "rubberDuck", "realDuck"];
+let pondScope = ["duck", "realDuck"];
 
 //////////////////PROBLEM 10////////////////////
 
@@ -144,11 +183,16 @@ let pondScope = ["duck", "sailorDuck", "rubberDuck", "realDuck"];
 //function which returns your name.
 
 //Code Here
+function outerFn () {
+  return () => "Douglas Caswell"
+};
 
 //Now save the result of invoking outerFn into a variable called innerFn.
 
 //Code Here
+var innerFn = outerFn();
 
 //Now invoke innerFn and save the result to a variable called finalResult.
 
 //Code Here
+var finalResult = innerFn();
